1f5bdfd1790b4342fbab720b7105701b91e3ddf5
jgo179 <jgo179@gmail.com>
2026-04-16 17:59:38 -0300

    added input and scroll area

---
+++ app/lib/storage.ts
@@ -0,0 +1,45 @@
+import type { Todo } from "../types/todo";
+
+export const List: Todo[] = [
+  {
+    id: 1,
+    description: "Learn TypeScript",
+    completed: false,
+  },
+  {
+    id: 2,
+    description: "Learn Next.js",
+    completed: true,
+  },
+  {
+    id: 3,
+    description: "Learn React",
+    completed: false,
+  },
+  {
+    id: 4,
+    description: "Learn Vue.js",
+    completed: false,
+  },
+  {
+    id: 5,
+    description: "Learn Angular",
+    completed: false,
+  },
+
+  {
+    id: 6,
+    description: "Learn Node.js",
+    completed: false,
+  },
+  {
+    id: 7,
+    description: "Learn Python",
+    completed: false,
+  },
+  {
+    id: 8,
+    description: "Learn Java",
+    completed: false,
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,97 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { List } from "./lib/storage";
+import type { Todo } from "./types/todo";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default function Home() {
+  const [todoList, setTodoList] = useState<Todo[]>(List);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Todo>();
+
+  const onSubmit = (data: Todo) => {
+    const description = data.description;
+    const id = todoList.length + 1;
+    setTodoList([
+      ...todoList,
+      {
+        id,
+        description,
+        completed: false,
+      },
+    ]);
+    reset();
+  };
+
+  const handleClick = (id: number) => {
+    const itemStatus = todoList[id - 1].completed;
+    setTodoList(
+      todoList.map((item) => {
+        if (item.id === id) {
+          item.completed = !itemStatus;
+        }
+        return item;
+      }),
+    );
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="">
+      <div className="flex flex-center mt-15 mb-15 justify-center">
+        <h1 className="text-2xl font-bold text-center">Listas de tarefas</h1>
+      </div>
+
+      <div className="max-w-4xl mx-auto">
+        <div className="border-2 rounded p-5">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-row mt-2 mb-2 gap-2 justify-between"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <Field orientation="horizontal">
+              <Input
+                {...register("description", {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 100,
+                })}
+                placeholder="Adicione um novo item"
+                aria-invalid={!!errors.description}
+              />
+              <Button type="submit">Adicionar</Button>
+            </Field>
+          </form>
         </div>
-      </main>
+
+        <div className="flex flex-col mt-2 border-2 rounded p-5 justify-center">
+          <ScrollArea className="h-[60vh]">
+            <ul className="flex flex-col-reverse mt-2 gap-2 ">
+              {todoList.map((item) => (
+                <li
+                  key={item.id}
+                  onClick={() => handleClick(item.id)}
+                  className="flex flex-row items-center justify-between cursor-pointer ml-5 mr-5 pt-5 pb-3 gap-2 border-b-2 hover:text-gray-400 "
+                >
+                  <span
+                    className={`${item.completed ? "line-through text-gray-400" : ""}`}
+                  >
+                    {item.description}
+                  </span>
+                  <span>{item.completed && "✅"}</span>
+                </li>
+              ))}
+            </ul>
+          </ScrollArea>
+        </div>
+      </div>
     </div>
   );
 }
